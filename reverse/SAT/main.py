e8fe5f376014bd1d7d0d7f65a2bb833f2fdbbd7f
zephrous <danielpoh3131@gmail.com>
2024-03-03 23:05:50 +0800

    update

---
+++ reverse/SAT/main.py
@@ -0,0 +1,31 @@
+variables = {
+    'a': 111,
+    'b': 115,
+    'c': 117,
+    'd': 123,
+    'e': 48,
+    'f': 114,
+    'g': 90,
+    'h': 95,
+    'i': 112,
+    'j': 51,
+    'k': 80,
+    'l': 112,
+    'm': 89,
+    'n': 95,
+    'o': 109,
+    'p': 121,
+    'q': 95,
+    'r': 115,
+    's': 52,
+    't': 118,
+    'u': 49,
+    'v': 111,
+    'w': 82,
+    'x': 125
+}
+
+for var, ascii_value in variables.items():
+    print(f"{var} = {chr(ascii_value)}")
+
+# flag: osu{0rZ_p3PpY_my_s4v1oR}
